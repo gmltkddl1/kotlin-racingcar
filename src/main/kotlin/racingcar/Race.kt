75270bb35aac50a51ed728d27226dddc288507ec
heesang.lee <sky1994601@hanmail.net>
2024-11-16 00:34:44 +0900

    refactor: step3 패키지 변경

--- src/main/kotlin/racingcar/Race.kt
+++ src/main/kotlin/racingcar/Race.kt
@@ -17,35 +17,6 @@ fun main() {
     }
 }
 
-class InputView() {
-    fun drawNumberOfCarInput() {
-        println("자동차 대수는 몇 대인가요?")
-    }
-
-    fun drawNumberOfTryInput() {
-        println("시도할 횟수는 몇 회인가요?")
-    }
-}
-
-class OutputView() {
-    fun drawRace(race: Race) {
-        val sb = StringBuilder()
-        for (i in 0 until race.carList.size) {
-            sb.append(drawCar(race.carList[i]))
-        }
-        println(sb.toString())
-    }
-
-    private fun drawCar(car: Car): String {
-        val sb = StringBuilder()
-        for (i in 1..car.moveCount) {
-            sb.append("-")
-        }
-        sb.append("\n")
-        return sb.toString()
-    }
-}
-
 class Race(numberOfCar: Int) {
     val carList: MutableList<Car> = mutableListOf()
 
