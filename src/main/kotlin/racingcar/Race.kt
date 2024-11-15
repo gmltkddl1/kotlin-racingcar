04278d3dd6aa419abc85479f397c6cca4e74b353
heesang.lee <sky1994601@hanmail.net>
2024-11-15 21:22:03 +0900

    feat: step3 자동차 경주 메인함수 구현

--- src/main/kotlin/racingcar/Race.kt
+++ src/main/kotlin/racingcar/Race.kt
@@ -1,5 +1,24 @@
 package racingcar
 
+fun main() {
+    println("자동차 대수는 몇 대인가요?")
+    val numberOfCar = readLine()!!.toInt()
+    println("시도할 횟수는 몇 회인가요?")
+    val numberOfTry = readLine()!!.toInt()
+    val race = Race(numberOfCar)
+    doRace(numberOfTry, race)
+}
+
+private fun doRace(
+    numberOfTry: Int,
+    race: Race,
+) {
+    for (i in 1..numberOfTry) {
+        race.process()
+        println(race.drawRace())
+    }
+}
+
 class Race(numberOfCar: Int) {
     private val carList: MutableList<Car> = mutableListOf()
 
